47ff71d54db91e320010517e7db1da09c44b0254
Aquib <aquibmahmood106@gmail.com.com>
2023-01-31 00:14:40 +0530

    Setting Up signIn and SignUp Pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,25 @@
 module.exports.profile=function(req,res){
     res.end('<h1> User Profile </h1>');
 }
+
+//Sign Up
+module.exports.signUp=function(req,res){
+    return res.render('user_sign_up',{
+        title:"EveryLivesMatters | Sign Up"
+    })
+}
+//Sign In
+module.exports.signIn=function(req,res){
+    return res.render('user_sign_in',{
+        title:"EveryLivesMatters | Sign In"
+    })
+}
+
+module.exports.create=function(req,res){
+
+}
+
+
+module.exports.createSession=function(req,res){
+    
+}
