b038fa36a116f052c7aff690aa033c4e049e5b20
Aquib <aquibmahmood106@gmail.com.com>
2023-01-30 23:20:10 +0530

    Setting up views

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app=express();
 const port=8000;
 
 app.use('/',require('./routes'));
+app.set('view engine','ejs');
+app.set('views', './views');
 
 app.listen(port,function(err){
     if(err){
