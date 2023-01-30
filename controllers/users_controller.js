3b9b8e72816a2d3ad3c67c9807cb0a02b0c2a778
Aquib <aquibmahmood106@gmail.com.com>
2023-01-31 02:14:53 +0530

    Setting Up More in SignIn and SignUp Pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
+// const { userInfo } = require("os");
+const User=require('../models/user');
+
 module.exports.profile=function(req,res){
     res.end('<h1> User Profile </h1>');
 }
@@ -16,10 +19,26 @@ module.exports.signIn=function(req,res){
 }
 
 module.exports.create=function(req,res){
+      if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+      }
 
+      User.findOne({email:req.body.email},function(err,user){
+        if(err){console.log('error in finding');return}
+
+        if(!user){
+            User.create(req.body, function(err, user){
+                if(err){console.log('error in create user while signUp');return}
+
+                return res.redirect('/users/sign-in');
+            })
+        }else{
+            return res.redirect('back');
+        }
+      });
 }
 
 
 module.exports.createSession=function(req,res){
-    
+    //
 }
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
-
+const cookieParser=require('cookie-parser');
 const app=express();
-
 const port=8000;
+
+
+app.use(express.urlencoded());
+
+app.use(cookieParser());
 
 app.use('/',require('./routes'));
 app.set('view engine','ejs');
